148d33f4d63c62521a34356264bf619eea694cec
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-04-09 11:19:48 +0300

    Fix problem with SECRET_KEY

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i@3p#esa9#je6au0p(w7f=h)*l!k#x%oib#y12(%4!yq2x56ml'
-    # os.environ.get('SECRET_KEY')
+SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
