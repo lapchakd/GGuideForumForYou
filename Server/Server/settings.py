17660a064743a2823db2ee214924148c8a501490
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-04-09 12:49:47 +0300

    fix problem with CSS

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets')
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'images'),
+]
