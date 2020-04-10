51ff7ffee6c09b8c71c66bf3d499fe895d78b266
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-04-10 13:36:00 +0300

    Add form for login

--- Server/GGuide/views.py
+++ Server/GGuide/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django import forms
 from django.views.generic.edit import ModelFormMixin
 
-from GGuide.models import SignUpForm
+from GGuide.models import SignUpForm, Userlogin
 
 
 def index(request):
@@ -32,3 +33,21 @@ def registration(request):
 def logout(request, next_page='index'):
 
     return redirect(next_page)
+
+
+def log_in(request):
+    success_url = "/"
+    if request.method == 'POST':
+        form = Userlogin(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect(success_url)
+            else:
+                return HttpResponse("Your account was inactive.")
+    else:
+        form = Userlogin()
+    return render(request, 'LogIn.html', {})
