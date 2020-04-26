4624315839b059716939ffddf6aa1ca02eec3013
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-04-26 13:33:06 +0300

    add model and form for comments

--- Server/GGuide/admin.py
+++ Server/GGuide/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Article, ProfileModel
+from .models import Article, ProfileModel, CommentsModel
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(ProfileModel)
+admin.site.register(CommentsModel)
 
--- Server/GGuide/models.py
+++ Server/GGuide/models.py
@@ -61,3 +61,15 @@ class FriendForm(forms.Form):
     email = forms.CharField(label='email', max_length=22)
 
 
+# class CommentsForm(forms.Form):
+#     comment = models.CharField(label='text', max_length=250)
+
+
+class CommentsModel(models.Model):
+    user_img = models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True)
+    text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f'{self.user}'
