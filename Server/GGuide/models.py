43a4a407710ea8790c6e08c74b67dd9689ff152a
Dima <61381794+Fester2004@users.noreply.github.com>
2020-04-27 12:56:29 +0300

    Comments (#38)
    * fix problem with migrations
    
    * add model and form for comments
    
    * fix mistakes

--- Server/GGuide/models.py
+++ Server/GGuide/models.py
@@ -64,3 +64,15 @@ class FriendForm(forms.Form):
     email = forms.CharField(label='email', max_length=22)
 
 
+# class CommentsForm(forms.Form):                                   now isn't using
+#     comment = models.CharField(label='text', max_length=250)
+
+
+class Comments(models.Model):
+    user_img = models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL)
+    text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f'{self.user}'
