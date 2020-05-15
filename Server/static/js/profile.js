53f9d1c1ac315e195183373bbd45eb1423265fad
Fester2004 <61381794+Fester2004@users.noreply.github.com>
2020-05-15 19:07:23 +0300

    add some changes

--- Server/static/js/profile.js
+++ Server/static/js/profile.js
@@ -1,15 +1,18 @@
 
 // load bar to the next LVl
 var articles = $('#articles_count').text();
-var procent
-if(articles <= 20){
-    procent = Math.floor((articles*100)/20);
-    $('.progress-bar').attr('style', 'width:'+ procent + "%").text(procent + "%");
-}else if (articles > 20 && articles < 50){
-    articles -=20;
-	procent = Math.floor((articles*100)/30);
-	$('.progress-bar').attr('style', 'width:'+ procent + "%").text(procent + "%");
-}else  {
-	$('.progress-bar').attr('style', 'width:100%').text("THE LAST LVL");
+var percent;
+count_articles(articles);
+function count_articles(articles){
+    if(articles <= 20){
+        percent = Math.floor((articles*100)/20);
+    }else if (articles > 20 && articles < 50){
+        articles -=20;
+        percent = Math.floor((articles*100)/30);
+    }else if (articles > 50)  {
+        $('.progress-bar').attr('style', 'width:100%').text("THE LAST LVL");
+        return;
+    }
+    $('.progress-bar').attr('style', 'width:'+ percent + "%").text(percent + "%");
 }
 // load bar to the next LVl
